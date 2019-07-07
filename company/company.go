0f2a07f322a0ec89364b2214cfbe975b8ddf1f1a
Amir Doroudian <amir.doroudian@hotmail.com>
2019-07-07 17:59:58 +0430

    Company - Echo All

--- company/company.go
+++ company/company.go
@@ -1,2 +1,48 @@
 package company
 
+import (
+	"net/http"
+	"text/template"
+	"drdn/bfp/config"
+)
+
+const echoAllCompanies = "SELECT * FROM company"
+
+// Company represents the company table structure
+type Company struct {
+	ID 		int
+	Name 	string
+	Type	string
+}
+
+var tmpl = template.Must(template.ParseGlob("forms/*"))
+
+// ShowAllCompanies displays all of the companies
+func ShowAllCompanies(w http.ResponseWriter, r *http.Request) {
+	db := config.DbConn()
+	selDB, err := db.Query(echoAllCompanies)
+	if err != nil {
+		panic(err)
+	}
+
+	com 	:= Company{}
+	comList := []Company{}
+
+	for selDB.Next() {
+		var _id 	int
+		var _name 	string
+		var _type	string
+		err = selDB.Scan(&_id, &_name, &_type)
+		if err != nil {
+			panic(err)
+		}
+		com.ID		= _id
+		com.Name 	= _name
+		com.Type 	= _type
+		comList 	= append(comList, com)
+	}
+	tmpl.ExecuteTemplate(w, "Company", comList)
+	defer db.Close()
+}
+
+
