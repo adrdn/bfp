34ec78c2ad27ac8de29d9257cfc049ceee06c694
Amir Doroudian <amir.doroudian@hotmail.com>
2019-07-08 14:01:20 +0430

    Company = CRUD is Completed

--- company/company.go
+++ company/company.go
@@ -6,7 +6,11 @@ import (
 	"drdn/bfp/config"
 )
 
-const echoAllCompanies = "SELECT * FROM company"
+const echoAllCompanies 	= "SELECT * FROM company"
+const echoOneCompany   	= "SELECT * FROM company WHERE id = ?"
+const updateCompany	   	= "UPDATE company SET name = ?, type = ? WHERE id = ?"
+const addNewCompany		= "INSERT INTO company (name, type) VALUES (?, ?)"
+const deleteCompany		= "DELETE FROM company WHERE id = ?"
 
 // Company represents the company table structure
 type Company struct {
@@ -16,10 +20,10 @@ type Company struct {
 }
 
 var tmpl = template.Must(template.ParseGlob("forms/*"))
+var db 	 = config.DbConn()
 
 // ShowAllCompanies displays all of the companies
 func ShowAllCompanies(w http.ResponseWriter, r *http.Request) {
-	db := config.DbConn()
 	selDB, err := db.Query(echoAllCompanies)
 	if err != nil {
 		panic(err)
@@ -45,4 +49,76 @@ func ShowAllCompanies(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 }
 
+// Edit edits the entity
+func Edit (w http.ResponseWriter, r *http.Request) {
+	rowID := r.URL.Query().Get("id")
+	selDB, err := db.Query(echoOneCompany, rowID)
+	if err != nil {
+		panic(err)
+	}
+	
+	com := Company{}
 
+	for selDB.Next() {
+		var _id   	int
+		var _name 	string
+		var _type 	string
+
+		err = selDB.Scan(&_id, &_name, &_type)
+		if err != nil {
+			panic(err)
+		}
+		com.ID 		= _id
+		com.Name 	= _name
+		com.Type 	= _type
+	}
+	tmpl.ExecuteTemplate(w, "Edit", com)
+	defer db.Close()
+}
+
+// Update updates the selected entity with given data
+func Update (w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		category := r.FormValue("type")
+		updatedData, err := db.Prepare(updateCompany)
+		if err != nil {
+			panic(err)
+		}
+		updatedData.Exec(name, category)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+// New represents the new entity page
+func New (w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "New", nil)
+}
+
+// Insert adds the new entity
+func Insert (w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		category := r.FormValue("type")
+		newData, err := db.Prepare(addNewCompany)
+		if err != nil {
+			panic(err)
+		}
+		newData.Exec(name, category)
+		defer db.Close()
+		http.Redirect(w, r, "/", 301)
+	}
+}
+
+// Delete drops the entity
+func Delete (w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	deletedData, err := db.Prepare(deleteCompany)
+	if err != nil {
+		panic(err)
+	}
+	deletedData.Exec(id)
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
