2fbfda5aeb918330990072125ac8ede08d40772e
Amir Doroudian <amir.doroudian@hotmail.com>
2019-07-13 16:59:57 +0430

    Admin - Flow - Create corresponding table upon new flow and delete that table upon deletion of the flow

--- flow/flow.go
+++ flow/flow.go
@@ -1,15 +1,20 @@
 package flow
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"net/http"
 	"text/template"
 
 	"adrdn/dit/config"
 )
 
-const echoAllFlows	= "SELECT * FROM flow"
-const newFlow		= "INSERT INTO flow (name) VALUES (?)"
-const deleteFlow	= "Delete FROM flow WHERE id = ?"
+const echoAllFlows		= "SELECT * FROM flow"
+const newFlow			= "INSERT INTO flow (name) VALUES (?)"
+const deleteFlow		= "DELETE FROM flow WHERE id = ?"
+//const deleteFlowTable 	= "DROP TABLE ?"
+const createTable		= "CREATE TABLE flow_"
 
 // Flow represents the flow structure
 type Flow struct {
@@ -50,8 +55,31 @@ func New (w http.ResponseWriter, r *http.Request) {
 // Insert adds the new flow
 func Insert (w http.ResponseWriter, r *http.Request) {
 	db := config.DbConn()
+	str := strings.Builder{}
+	str.WriteString(createTable)
+	var steps []string
+
 	if r.Method == "POST" {
 		name := r.FormValue("name")
+		nameUpper := strings.ToUpper(name)
+		numberString := r.FormValue("number")
+		number, _ := strconv.Atoi(numberString)
+
+		// define 'CTEATE TABLE flow_NAME' (
+		str.WriteString(nameUpper + " (")
+		
+		// define coloumn names
+		for i := 1; i <= number; i++ {
+			steps = append(steps, "step" + strconv.Itoa(i))
+		}
+		
+		// define query of columns with their data type
+		columns := strings.Join(steps, " varchar(255), ")
+		str.WriteString(columns + " varchar(255))")
+
+		// create a table with the given name and column number
+		db.Query(str.String())
+		
 		insForm, err := db.Prepare(newFlow)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
@@ -66,11 +94,21 @@ func Insert (w http.ResponseWriter, r *http.Request) {
 func Delete (w http.ResponseWriter, r *http.Request) {
 	db := config.DbConn()
 	id := r.URL.Query().Get("id")
-	delForm, err := db.Prepare(deleteFlow)
+	name := r.URL.Query().Get("name")
+
+	_, err := db.Query("DROP TABLE " + "flow_" + strings.ToUpper(name))
 	if err != nil {
+		fmt.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
-	delForm.Exec(id)
+
+	delRow, err := db.Prepare(deleteFlow)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	delRow.Exec(id)
+
 	defer db.Close()
 	http.Redirect(w, r, "/admin/flow", 301)
 }
